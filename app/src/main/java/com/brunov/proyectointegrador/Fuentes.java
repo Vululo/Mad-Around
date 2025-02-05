d6ed0bfe60c3f72e691ee7e3970fad7cf557a92c
KVinny <kevinluislaurentelopez@gmail.com>
2025-02-05 12:39:49 +0100

    Toggle of the buttons added, new appIcon

--- app/src/main/java/com/brunov/proyectointegrador/Fuentes.java
+++ app/src/main/java/com/brunov/proyectointegrador/Fuentes.java
@@ -2,61 +2,18 @@ package com.brunov.proyectointegrador;
 
 public class Fuentes {
 
-    /*private String ID;
-    private String descClasificacion;
-    private String codBarrio;*/
     private String BARRIO;
-    /*private String codDistrito;
-    private String distrito;*/
     private String ESTADO;
-    /*private double coordGisX;
-    private double coordGisY;
-    private String sistemaCoord;*/
     private double LATITUD;
     private double LONGITUD;
-    //private String tipoVia;
     private String NOM_VIA;
-    /*private String numVia;
-    private String codPostal;
-    private String direccionAux;
-    private String ndp;
-    private String fechaInstalacion;
-    private String codigoInterno;
-    private String contratoCod;
-    private String ubicacion;*/
     private String USO;
-    /*private String modelo;
-
-    // Getters y Setters
-    public String getId() { return ID; }
-    public void setId(String id) { this.ID = id; }
-
-    public String getDescClasificacion() { return descClasificacion; }
-    public void setDescClasificacion(String descClasificacion) { this.descClasificacion = descClasificacion; }
-
-    public String getCodBarrio() { return codBarrio; }
-    public void setCodBarrio(String codBarrio) { this.codBarrio = codBarrio; }*/
 
     public String getBarrio() { return BARRIO; }
     public void setBarrio(String BARRIO) { this.BARRIO = BARRIO; }
 
-    /*public String getCodDistrito() { return codDistrito; }
-    public void setCodDistrito(String codDistrito) { this.codDistrito = codDistrito; }
-
-    public String getDistrito() { return distrito; }
-    public void setDistrito(String distrito) { this.distrito = distrito; }*/
-
     public String getEstado() { return ESTADO; }
     public void setEstado(String ESTADO) { this.ESTADO = ESTADO; }
-
-    /*public double getCoordGisX() { return coordGisX; }
-    public void setCoordGisX(double coordGisX) { this.coordGisX = coordGisX; }
-
-    public double getCoordGisY() { return coordGisY; }
-    public void setCoordGisY(double coordGisY) { this.coordGisY = coordGisY; }
-
-    public String getSistemaCoord() { return sistemaCoord; }
-    public void setSistemaCoord(String sistemaCoord) { this.sistemaCoord = sistemaCoord; }*/
 
     public double getLatitud() { return LATITUD; }
     public void setLatitud(double LATITUD) { this.LATITUD = LATITUD; }
@@ -64,40 +21,9 @@ public class Fuentes {
     public double getLongitud() { return LONGITUD; }
     public void setLongitud(double LONGITUD) { this.LONGITUD = LONGITUD; }
 
-    //public String getTipoVia() { return tipoVia; }
-    //public void setTipoVia(String tipoVia) { this.tipoVia = tipoVia; }
-
     public String getNomVia() { return NOM_VIA; }
     public void setNomVia(String nomVia) { this.NOM_VIA = nomVia; }
 
-    /*public String getNumVia() { return numVia; }
-    public void setNumVia(String numVia) { this.numVia = numVia; }
-
-    public String getCodPostal() { return codPostal; }
-    public void setCodPostal(String codPostal) { this.codPostal = codPostal; }
-
-    public String getDireccionAux() { return direccionAux; }
-    public void setDireccionAux(String direccionAux) { this.direccionAux = direccionAux; }
-
-    public String getNdp() { return ndp; }
-    public void setNdp(String ndp) { this.ndp = ndp; }
-
-    public String getFechaInstalacion() {return fechaInstalacion;}
-
-    public void setFechaInstalacion(String fechaInstalacion) {this.fechaInstalacion = fechaInstalacion;}
-
-    public String getCodigoInterno() { return codigoInterno; }
-    public void setCodigoInterno(String codigoInterno) { this.codigoInterno = codigoInterno; }
-
-    public String getContratoCod() { return contratoCod; }
-    public void setContratoCod(String contratoCod) { this.contratoCod = contratoCod; }
-
-    public String getUbicacion() { return ubicacion; }
-    public void setUbicacion(String ubicacion) { this.ubicacion = ubicacion; }*/
-
     public String getUso() { return USO; }
     public void setUso(String USO) { this.USO = USO; }
-
-    /*public String getModelo() { return modelo; }
-    public void setModelo(String modelo) { this.modelo = modelo; }*/
 }
